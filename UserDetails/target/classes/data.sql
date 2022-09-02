9dd3849c4c5bc2a89e071fac2afd28b8483e7024
ipatrickcog <97034236+ipatrickcog@users.noreply.github.com>
2022-09-02 12:54:51 +0200

    Delete UserDetails directory

--- UserDetails/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO USERS (username, password, fullname, age, dob, address, email) VALUES
- ('martinpullman9', 'martpull123', 'Martin Pullman', 23, '1999-04-27', 'Springweg 25', 'martpulls@gmail.com'),
- ('johncuisine00', 'jcuzz987', 'John Cuisine', 24, '1998-05-17', 'Haverstraat 125', 'john_cooks@hotmail.com'),
- ('zackabe5', 'zackyabraham77', 'Zac Abbey', 25, '1996-07-07', 'Herenstraat 42', 'izaackey@gmail.com');
